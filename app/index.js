0f681ee9fb3279287d9439058beee30a5014b756
Tender <tender121995@gmail.com>
2023-04-01 16:13:48 +0300

    added sort

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { getMovies } from "./repository.js";
+import { getMovies, getSortMovies, getByGenre } from "./repository.js";
 import express, { json, request, response } from "express";
 import cors from "cors";
 
@@ -10,6 +10,22 @@ app.use(cors());
 app.get("/api/v1/movies/all", async (request, response) => {
   let movies = await getMovies();
   response.status(200).json(movies);
+});
+
+app.get("/api/v1/movies/sort/:field", async (request, response) => {
+  console.log(request.params);
+  let movies = await getSortMovies(request.params.field);
+  response.status(200).json(movies);
+});
+
+app.get("/api/v1/movies/find/genre/:genre", async (request, response) => {
+  let movies = await getByGenre(request.params.genre);
+  console.log(request.params.genre);
+  if (movies !== null) {
+    response.status(200).json(movies);
+  } else {
+    response.status(400).json({ message: "Filmul nu este in baza de date " });
+  }
 });
 
 app.listen(3000, () => {
--- app/repository.js
+++ app/repository.js
@@ -13,3 +13,28 @@ export function getMovies() {
     });
   });
 }
+
+export async function getSortMovies(field) {
+  let movies = await getMovies();
+  for (let i = 0; i < movies.length - 1; i++) {
+    for (let j = i + 1; j < movies.length; j++) {
+      if (movies[i][field] > movies[j][field]) {
+        let aux = movies[i];
+        movies[i] = movies[j];
+        movies[j] = aux;
+      }
+    }
+  }
+  return movies;
+}
+
+export async function getByGenre(genre) {
+  let movies = await getMovies();
+  console.log(genre);
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].genre.toLowerCase() === genre.toLowerCase()) {
+      return movies[i];
+    }
+  }
+  return null;
+}
